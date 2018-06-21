f501fe576b27a29fd53e97dd989eeb947beb8875
Cassy Myers <cassymyers@gmail.com>
2018-06-21 09:33:42 -0700

    change roots

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,28 +1,27 @@
 class DocsController < ApplicationController
   before_action :find_doc, only: [:edit, :update, :destroy]
+
   def index
-    @docs = Doc.all.order('created_at DESC')
+    @docs = Doc.where(user_id: current_user)
   end
 
   def show
   end
 
   def new
-    @doc = Doc.new
+    @doc = current_user.docs.build
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
     if @doc.save
       redirect_to docs_path
     else
       render :new
     end
-
   end
 
   def edit
-
   end
 
   def update
